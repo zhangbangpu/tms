99b3e4c7a722c06bf111938f46b45b0e215feae2
shuxiaoxiao <971322430@qq.com>
2016-08-02 23:06:08 +0800

    站点，区域管理以及班线管理

---
+++ tms/tms.server/src/main/java/com/chinaway/tms/basic/dao/LineMapper.java
@@ -0,0 +1,8 @@
+package com.chinaway.tms.basic.dao;
+
+import com.chinaway.tms.basic.model.Line;
+import com.chinaway.tms.core.BaseMapper;
+
+public interface LineMapper extends BaseMapper<Line, Integer> {
+	
+}
