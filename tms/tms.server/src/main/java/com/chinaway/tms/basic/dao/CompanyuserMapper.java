5929b5a42d645828840a0aeb28422d651fc8a4eb
shuxiaoxiao <971322430@qq.com>
2016-08-03 15:51:40 +0800

    站点，区域管理以及班线管理

---
+++ tms/tms.server/src/main/java/com/chinaway/tms/basic/dao/CompanyuserMapper.java
@@ -0,0 +1,8 @@
+package com.chinaway.tms.basic.dao;
+
+import com.chinaway.tms.basic.model.Companyuser;
+import com.chinaway.tms.core.BaseMapper;
+
+public interface CompanyuserMapper extends BaseMapper<Companyuser, Integer> {
+	
+}
