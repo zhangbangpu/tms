0e1934c682e263c9788d772cf6731ba912de5b96
shuxiaoxiao <971322430@qq.com>
2016-08-03 15:52:55 +0800

    站点，区域管理以及班线管理

--- tms/tms.web/src/main/java/com/chinaway/tms/basic/controller/LineController.java
+++ tms/tms.web/src/main/java/com/chinaway/tms/basic/controller/LineController.java
@@ -2,11 +2,13 @@ package com.chinaway.tms.basic.controller;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.chinaway.tms.basic.model.Line;
 import com.chinaway.tms.basic.service.LineService;
 import com.chinaway.tms.utils.json.JsonUtil;
@@ -32,7 +34,6 @@ public class LineController {
     	Map<String, String> argsMap = new HashMap<String, String>();
     	try {
     		lineService.insert(line);
-//			ret = siteService.addSite(site);
     		argsMap.put("status", "true");
 			argsMap.put("msg", "add Line success!");
 		} catch (Exception e) {
