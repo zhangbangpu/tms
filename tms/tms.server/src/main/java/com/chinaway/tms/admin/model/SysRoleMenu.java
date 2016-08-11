624d3d8bb253e60435f86dc0fc34326839b83612
shuxiaoxiao <971322430@qq.com>
2016-08-11 10:14:42 +0800

    修改角色id大小写问题

--- tms/tms.server/src/main/java/com/chinaway/tms/admin/model/SysRoleMenu.java
+++ tms/tms.server/src/main/java/com/chinaway/tms/admin/model/SysRoleMenu.java
@@ -4,22 +4,22 @@ import java.io.Serializable;
 
 public class SysRoleMenu implements Serializable {
 
+	private static final long serialVersionUID = -7004789906044546976L;
+	
 	private Integer roleid;// 角色id
 	private Integer menuid;// 菜单id
-
-	public Integer getroleid() {
-		return this.roleid;
+	
+	public Integer getRoleid() {
+		return roleid;
 	}
-
-	public void setroleid(Integer roleid) {
+	public void setRoleid(Integer roleid) {
 		this.roleid = roleid;
 	}
-
 	public Integer getMenuid() {
-		return this.menuid;
+		return menuid;
 	}
-
 	public void setMenuid(Integer menuid) {
 		this.menuid = menuid;
 	}
+
 }
--- tms/tms.server/src/main/java/com/chinaway/tms/admin/model/SysUserRole.java
+++ tms/tms.server/src/main/java/com/chinaway/tms/admin/model/SysUserRole.java
@@ -3,7 +3,9 @@ package com.chinaway.tms.admin.model;
 import java.io.Serializable;
 
 public class SysUserRole implements Serializable {
+		private static final long serialVersionUID = 1579467190398275039L;
 	
-		private Integer userid;//   用户id	private Integer roleid;//   角色id	public Integer getUserid() {	    return this.userid;	}	public void setUserid(Integer userid) {	    this.userid=userid;	}	public Integer getRoleid() {	    return this.roleid;	}	public void setRoleid(Integer roleid) {	    this.roleid=roleid;	}
+	private Integer userid;  //   用户id	private Integer roleid;  //   角色id
+		public Integer getUserid() {	    return this.userid;	}	public void setUserid(Integer userid) {	    this.userid=userid;	}	public Integer getRoleid() {	    return this.roleid;	}	public void setRoleid(Integer roleid) {	    this.roleid=roleid;	}
 }
 
