629f0ed1eefd2e05bd6f30547fdc12a7f6e5944e
shuxiaoxiao <971322430@qq.com>
2016-08-18 17:24:21 +0800

    商品和订单后台开发

--- tms/tms.server/src/main/java/com/chinaway/tms/basic/dao/CpmdMapper.java
+++ tms/tms.server/src/main/java/com/chinaway/tms/basic/dao/CpmdMapper.java
@@ -1,8 +1,18 @@
 package com.chinaway.tms.basic.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import com.chinaway.tms.basic.model.Cpmd;
 import com.chinaway.tms.core.BaseMapper;
 
 public interface CpmdMapper extends BaseMapper<Cpmd, Integer> {
+
+	/**
+	 * 查询所有商品信息
+	 * @param argsMap
+	 * @return
+	 */
+	List<Cpmd> selectAllCpmdByCtn(Map<String, Object> argsMap);
 	
 }
