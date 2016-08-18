1b14a13245799c1a1331bd1c86de924257df0619
shuxiaoxiao <971322430@qq.com>
2016-08-18 17:26:11 +0800

    商品和订单后台开发

--- tms/tms.server/src/main/java/com/chinaway/tms/basic/service/CpmdService.java
+++ tms/tms.server/src/main/java/com/chinaway/tms/basic/service/CpmdService.java
@@ -1,8 +1,13 @@
 package com.chinaway.tms.basic.service;
 
+import java.util.List;
+import java.util.Map;
+
 import com.chinaway.tms.basic.model.Cpmd;
 import com.chinaway.tms.core.BaseService;
 
 public interface CpmdService extends BaseService<Cpmd, Integer> {
+
+	List<Cpmd> selectAllCpmdByCtn(Map<String, Object> argsMap);
 	
 }
