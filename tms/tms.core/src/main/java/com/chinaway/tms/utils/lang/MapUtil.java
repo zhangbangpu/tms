e013efe7394ca7564fc777819cef70cd0fb9ea89
shuxiaoxiao <971322430@qq.com>
2016-08-23 18:32:07 +0800

    运单审核

---
+++ tms/tms.core/src/main/java/com/chinaway/tms/utils/lang/MapUtil.java
@@ -0,0 +1,53 @@
+package com.chinaway.tms.utils.lang;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * map 工具类
+ */
+public class MapUtil {
+	
+	public static Object mapToObject(Map<String, Object> map, Class<?> beanClass) throws Exception {    
+        if (map == null)   
+            return null;    
+  
+        Object obj = beanClass.newInstance();  
+  
+        BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());    
+        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();    
+        for (PropertyDescriptor property : propertyDescriptors) {  
+            Method setter = property.getWriteMethod();    
+            if (setter != null) {  
+                setter.invoke(obj, map.get(property.getName()));   
+            }  
+        }  
+  
+        return obj;  
+    }  
+	
+	public static Map<String, Object> objectToMap(Object obj) throws Exception {
+		if (obj == null)
+			return null;
+
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+		PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+		for (PropertyDescriptor property : propertyDescriptors) {
+			String key = property.getName();
+			if (key.compareToIgnoreCase("class") == 0) {
+				continue;
+			}
+			Method getter = property.getReadMethod();
+			Object value = getter != null ? getter.invoke(obj) : null;
+			map.put(key, value);
+		}
+
+		return map;
+	}
+}
