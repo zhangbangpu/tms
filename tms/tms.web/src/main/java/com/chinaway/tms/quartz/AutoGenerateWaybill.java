08b79c6c1979449fd5e8d16190952921ef5cca75
shuxiaoxiao <971322430@qq.com>
2016-08-25 19:24:28 +0800

    定时任务

--- tms/tms.web/src/main/java/com/chinaway/tms/quartz/AutoGenerateWaybill.java
+++ tms/tms.web/src/main/java/com/chinaway/tms/quartz/AutoGenerateWaybill.java
@@ -1,46 +1,58 @@
 package com.chinaway.tms.quartz;
 
-import org.springframework.stereotype.Component;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.chinaway.tms.basic.model.Orders;
+import com.chinaway.tms.basic.service.OrdersService;
 
-@Component
 public class AutoGenerateWaybill {
 
-//	private final Logger logger = LoggerFactory.getLogger(getClass());
-//	
-//	@Autowired
-//	private OrdersService ordersService;
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Autowired
+	private OrdersService ordersService;
+	
+	public OrdersService getOrdersService() {
+		return ordersService;
+	}
 
-	
-	
+	public void setOrdersService(OrdersService ordersService) {
+		this.ordersService = ordersService;
+	}
+
 	public void execute() {
 		System.out.println("quartz success!");
-//		try {
-//			Map<String, Object> map = new HashMap<String, Object>();
-//			map.put("status", "0");
-//			map.put("state", "0");
-//			List<Orders> ordersList = ordersService.selectAllOrdersByCtn(map);
-//			for (Orders orders : ordersList) {
-//				Map<String, Object> argsMap = new HashMap<String, Object>();
-//				argsMap.put("id", orders.getId());
-//				int ret = ordersService.generateWaybill(argsMap);
-//				if(ret == 0){
-//					// TODO 打日志
-//					System.out.println("orders=" + orders.getId());
-//				}
-//			}
-//
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		} finally {
-//			try {
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			} finally {
-//				try {
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		}
+		try {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("status", "0");
+			map.put("state", "0");
+			List<Orders> ordersList = ordersService.selectAllOrdersByCtn(map);
+			for (Orders orders : ordersList) {
+				Map<String, Object> argsMap = new HashMap<String, Object>();
+				argsMap.put("id", orders.getId());
+				List<String> waybills = ordersService.generateWaybill(orders);
+				if (null != waybills && waybills.size() > 0) {
+					// TODO 打日志
+					System.out.println("orders=" + orders.getId());
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
